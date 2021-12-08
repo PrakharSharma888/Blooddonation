f2d0f6d6badfd5403d6efa1d05d0c9c57df23c89
Prakhar Sharma <prakharentp2002@gmail.com>
2021-12-08 19:47:07 +0530

    Update settings.py

--- bloodsbanks/settings.py
+++ bloodsbanks/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
